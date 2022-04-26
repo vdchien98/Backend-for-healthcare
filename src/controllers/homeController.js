ce11406c019b035cfa57c9bb7086deda7c727e0b
vdchien98 <vudangchien98@gmail.com>
2022-04-27 01:23:57 +0700

    sixthcommit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -38,10 +38,44 @@ let displayGetCRUD = async (req, res) => {
     });
 };
 
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+        console.log(userData);
+        return res.render('editCRUD.ejs', {
+            user: userData,
+        });
+    } else {
+        return res.send('User not found!!');
+    }
+};
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers,
+    });
+};
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('delete user successfully');
+    } else {
+        return res.send('User not found!!');
+    }
+};
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 };
